46acaaf04d263b5a3d2ded2c6591168b70393682
Gregory Danielson <gregdan3@uab.edu>
2020-03-06 08:56:24 -0600

    Implement 0xFx15: Set value of delay timer to value of V[x]

--- chip8.c
+++ chip8.c
@@ -209,6 +209,9 @@ void emulate_cycle(struct CHIP8* self)
             self->V[(self->opcode & 0x0F00) >> 8] = self->delay_timer;
             break;
         case 0x000A: // wait for keypress, store in 0r00
+        case 0x0015: // set delay timer to value of 0r00
+            self->delay_timer = self->V[(self->opcode & 0x0F00) >> 8];
+            break;
         }
         self->pc += 2;
         break;
