6f67cc6bb66fda3a3a5e0287f1111058a7162fe7
Gregory Danielson <gregdan3@uab.edu>
2020-09-27 17:18:14 -0500

    Implement 0xFxy1: V[x] |= V[y]

--- chip8.c
+++ chip8.c
@@ -176,7 +176,8 @@ void emulate_cycle(struct CHIP8* self)
         case 0x0000: // assign register 0r00 to value of register 00r0
             self->V[(self->opcode & 0x0F00) >> 8] = self->V[(self->opcode & 0x00F0) >> 4];
             break;
-        case 0x0001:
+        case 0x0001: // assign register 0r00 to 0r00 bitwise OR 00r0
+            self->V[(self->opcode & 0x0F00) >> 8] |= self->V[(self->opcode & 0x00F0) >> 4];
             break;
         case 0x0002:
             break;
